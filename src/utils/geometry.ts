af8cae6dcf931c0b9980696ef87254eddddf2821
sealleci <ratratkin@gmail.com>
2024-04-09 20:37:13 +0800

    Optimized the formula

--- src/utils/geometry.ts
+++ src/utils/geometry.ts
@@ -4,7 +4,6 @@
  */
 function calcPolygonArea(coords: [number, number][], isRawResult: boolean = false): number {
     let result: number = 0
-    const minY: number = coords.reduce((min, coord) => Math.min(min, coord[1]), Infinity)
 
     for (let i = 0; i < coords.length; i += 1) {
         result += (
@@ -12,7 +11,7 @@ function calcPolygonArea(coords: [number, number][], isRawResult: boolean = fals
             - coords[(i + 1) % coords.length][0]
         ) * (
                 coords[(i + 1) % coords.length][1]
-                + coords[i][1] - minY * 2
+                + coords[i][1]
             )
     }
 
@@ -28,32 +27,28 @@ function calcPolygonArea(coords: [number, number][], isRawResult: boolean = fals
  */
 function calcPolygonCentroid(coords: [number, number][]): [number, number] {
     const area = calcPolygonArea(coords, true)
-    const minX = coords.reduce((min, coord) => Math.min(min, coord[0]), Infinity)
-    const minY = coords.reduce((min, coord) => Math.min(min, coord[1]), Infinity)
     let centerX: number = 0
     let centerY: number = 0
 
     for (let i = 0; i < coords.length; i += 1) {
         const factor = (
-            (coords[i][0] - minX)
-            * (coords[(i + 1) % coords.length][1] - minY)
-            - (coords[(i + 1) % coords.length][0] - minX)
-            * (coords[i][1] - minY)
+            coords[i][0]
+            * coords[(i + 1) % coords.length][1]
+            - coords[(i + 1) % coords.length][0]
+            * coords[i][1]
         )
 
         centerX += (
             coords[i][0]
             + coords[(i + 1) % coords.length][0]
-            - minX * 2
         ) * factor
         centerY += (
             coords[i][1]
             + coords[(i + 1) % coords.length][1]
-            - minY * 2
         ) * factor
     }
 
-    return [centerX / (6 * area) + minX, centerY / (6 * area) + minY]
+    return [centerX / (6 * area), centerY / (6 * area)]
 }
 
 export { calcPolygonArea, calcPolygonCentroid }
